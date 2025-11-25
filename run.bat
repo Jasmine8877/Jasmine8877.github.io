4c43ec072c1179ce9f6f3d4875186be63eab2d6b
Jasmine8877 <M18736735877@163.com>
2025-11-25 17:16:53 +0800

    Add new posts on out-of-distribution learning and graph domain adaptation; update batch scripts for Jekyll server management

---
+++ run.bat
@@ -0,0 +1,13 @@
+@echo off
+echo ---------------------------------------
+echo   Starting Jekyll Server on Windows...
+echo ---------------------------------------
+echo.
+
+
+cd /d C:\baby\personal_page\Jasmine8877.github.io
+
+
+bundle exec jekyll serve
+
+pause
